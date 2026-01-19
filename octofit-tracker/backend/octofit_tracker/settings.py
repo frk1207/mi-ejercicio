c046139267e548621ca0b54222ada5192b9ecfa6
frk1207 <134947096+frk1207@users.noreply.github.com>
2026-01-19 16:28:51 +0000

    Configuración de REST Framework y URLs de Codespace

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure--68jf(b_%+b-17+9)r&w3th3*(s&u689!j+ijdnoy5tid!8drr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+codespace_host = f"{CODESPACE_NAME}-8000.app.github.dev" if CODESPACE_NAME else None
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if codespace_host:
+    ALLOWED_HOSTS.append(codespace_host)
 
 
 # Application definition
